da2448fb9bbc2e6b2b7d4b119f35b0fb46a53b24
amrit gautam <agautam@localhost.localdomain>
2021-03-26 14:52:40 -0400

    automatic trigger class for CMUP11 and CMUP10 and v0 decision as suggested by evgeny

--- Tasks/Run3/upcForward.cxx
+++ Tasks/Run3/upcForward.cxx
@@ -19,7 +19,6 @@ alien_cp alien:/alice/data/2015/LHC15o/000246392/pass5_lowIR/PWGZZ/Run3_Conversi
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
-#include "AnalysisDataModel/TrackSelectionTables.h"
 #include "AnalysisDataModel/EventSelection.h"
 #include <TH1D.h>
 #include <TH2D.h>
@@ -33,28 +32,35 @@ using namespace o2::framework::expressions;
 #define mmuon 0.1057 //mass of muon
 struct UPCForward {
   //defining histograms
-  OutputObj<TH1D> hMass{TH1D("hMass", ";#it{m_{#mu#mu}}, GeV/c^{2};", 500, 0, 10)};
-  OutputObj<TH1D> hPt{TH1D("hPt", ";#it{p_{t}}, GeV/c;", 500, 0., 5.)};
-  OutputObj<TH1D> hPtsingle_muons{TH1D("hPtsingle_muons", ";#it{p_{t}}, GeV/c;", 500, 0., 5.)};
-  OutputObj<TH1D> hPx{TH1D("hPx", ";#it{p_{x}}, GeV/c;", 500, -5., 5.)};
-  OutputObj<TH1D> hPy{TH1D("hPy", ";#it{p_{y}}, GeV/c;", 500, -5., 5.)};
-  OutputObj<TH1D> hPz{TH1D("hPz", ";#it{p_{z}}, GeV/c;", 500, -5., 5.)};
-  OutputObj<TH1D> hRap{TH1D("hRap", ";#it{y},..;", 500, -10., 10.)};
-  OutputObj<TH1D> hEta{TH1D("hEta", ";#it{y},..;", 500, -10., 10.)};
-  OutputObj<TH1D> hCharge{TH1D("hCharge", ";#it{charge},..;", 500, -10., 10.)};
-  OutputObj<TH1D> hSelectionCounter{TH1D("hSelectionCounter", ";#it{Selection},..;", 30, 0., 30.)};
-  OutputObj<TH1D> hPhi{TH1D("hPhi", ";#it{#Phi},;", 500, -6., 6.)};
+  HistogramRegistry registry{
+    "registry",
+    {{"hMass", ";#it{m_{#mu#mu}}, GeV/c^{2};", {HistType::kTH1D, {{500, 0, 10}}}},
+     {"hPt", ";#it{p_{t}}, GeV/c;", {HistType::kTH1D, {{500, 0., 5.}}}},
+     {"hPtsingle_muons", ";#it{p_{t}}, GeV/c;", {HistType::kTH1D, {{500, 0., 5.}}}},
+     {"hPx", ";#it{p_{x}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
+     {"hPy", ";#it{p_{y}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
+     {"hPz", ";#it{p_{z}}, GeV/c;", {HistType::kTH1D, {{500, -5., 5.}}}},
+     {"hRap", ";#it{y},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
+     {"hEta", ";#it{y},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
+     {"hCharge", ";#it{charge},..;", {HistType::kTH1D, {{500, -10., 10.}}}},
+     {"hSelectionCounter", ";#it{Selection},..;", {HistType::kTH1I, {{30, 0., 30.}}}},
+     {"hPhi", ";#it{#Phi},;", {HistType::kTH1D, {{500, -6., 6.}}}}}};
 
   void init(o2::framework::InitContext&)
   {
-    TString SelectionCuts[6] = {"NoSelection", "CMup11Trigger", "twotracks", "oppositecharge", "-2.5<Eta<-4", "Pt<1"};
+    //TString SelectionCuts[8] = {"NoSelection", "CMup11and10Trigger","V0Selection","FDSelection", "twotracks", "oppositecharge", "-2.5<Eta<-4", "Pt<1"};
+
+    /* // commenting out this part now as histogram registry do not seem to suppor the binlabel usign AxisSpec() or any other method
     for (int i = 0; i < 6; i++) {
-      hSelectionCounter->GetXaxis()->SetBinLabel(i + 1, SelectionCuts[i].Data());
-    }
+      registry.GetXaxis().(HIST("hSelectionCounter",SetBinLabel(i + 1, SelectionCuts[i].Data())));
+    }*/
   }
-  void process(soa::Join<aod::BCs,aod::Run2BCInfos>::iterator const& bc, aod::Muons const& tracksMuon)
+  // old
+  void process(soa::Join<aod::BCs, aod::Run2BCInfos>::iterator const& bc, aod::Muons const& tracksMuon)
+ //new
+//  void process(soa::Join<aod::BCs, aod::BcSels>::iterator const& bc, aod::Muons const& tracksMuon)
   {
-    hSelectionCounter->Fill(0);
+    registry.fill(HIST("hSelectionCounter"), 0);
 
     int iMuonTracknumber = 0;
     TLorentzVector p1, p2, p;
@@ -63,28 +69,55 @@ struct UPCForward {
     /*this code below is suggested by evgeny.
     this code is now hardcoded for runs  246391, 246392 for CMUP11
     and 244980, 244982, 244983, 245064, 245066, 245068 for CMUP10*/
-    uint64_t classIndexMUP = -1;
+  /*  uint64_t classIndexMUP = -1;
     Int_t iRunNumber = bc.runNumber();
 
-    if (iRunNumber==246391 || iRunNumber==246392) {
+    if (iRunNumber == 246391 || iRunNumber == 246392) {
       classIndexMUP = 51; //CMUP11
-    } else if (iRunNumber==246980 || iRunNumber==246982 || iRunNumber==246983) {
+    } else if (iRunNumber == 246980 || iRunNumber == 246982 || iRunNumber == 246983) {
       classIndexMUP = 88; //CMUP10
-    } else if (iRunNumber==245064 || iRunNumber==245066 || iRunNumber==245068){
-      classIndexMUP=62; //CMUP10
+    } else if (iRunNumber == 245064 || iRunNumber == 245066 || iRunNumber == 245068) {
+      classIndexMUP = 62; //CMUP10
     }
-    if (classIndexMUP==-1) {
+    if (classIndexMUP == -1) {
       return;
-    }
-    //selecting CMUP10 and CMUP11 events selection
-    bool isMUP11fired = bc.triggerMaskNext50() & (1ull << classIndexMUP-50);
+    }*/
+  // V0 and FD information
+    bool isBeamBeamV0A = bc.bbV0A();
+    bool isBeamGasV0A  = bc.bgV0A();
+    bool isBeamBeamV0C = bc.bbV0C();
+    bool isBeamGasV0C  = bc.bgV0C();
 
-    if (!isMUP11fired) {
+    bool isBeamBeamFDA = bc.bbFDA();
+    bool isBeamGasFDA  = bc.bgFDA();
+    bool isBeamBeamFDC = bc.bbFDC();
+    bool isBeamGasFDC  = bc.bgFDC();
+
+    //CCUP10 and CCUP11 information
+    bool isV0Selection = isBeamBeamV0A || isBeamGasV0A ||isBeamGasV0C;
+    bool isFDSelection = isBeamBeamFDA || isBeamGasFDA ||isBeamBeamFDC ||isBeamGasFDC;
+
+
+    bool iskMUP11fired = bc.alias()[kMUP11]; //bc.triggerMaskNext50() & (1ull << (classIndexMUP - 50));
+    bool iskMUP10fired =  bc.alias()[kMUP10];
+
+    if (!iskMUP11fired) || !iskMUP10fired) {
       return;
     }
-    hSelectionCounter->Fill(1);
+    registry.fill(HIST("hSelectionCounter"), 1);
+
+    if (!isV0Selection){
+      return;
+    }
+    registry.fill(HIST("hSelectionCounter"), 2);
+
+    if (!isFDSelection){
+      return;
+    }
+    registry.fill(HIST("hSelectionCounter"), 3);
+
     for (auto& muon : tracksMuon) {
-      hCharge->Fill(muon.sign());
+      registry.fill(HIST("hCharge"), muon.sign());
       iMuonTracknumber++;
       if (muon.sign() > 0) {
         p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
@@ -98,36 +131,37 @@ struct UPCForward {
     if (iMuonTracknumber != 2) {
       return;
     }
-    hSelectionCounter->Fill(2);
+    registry.fill(HIST("hSelectionCounter"), 4);
     if (!ispositive || !isnegative) {
       return;
     }
-    hSelectionCounter->Fill(3);
+    registry.fill(HIST("hSelectionCounter"), 5);
 
     if (-4 < p1.Eta() < -2.5 || -4 < p2.Eta() < -2.5) {
       return;
     }
-    hSelectionCounter->Fill(4);
+    registry.fill(HIST("hSelectionCounter"), 6);
     p = p1 + p2;
     if (p.Pt() > 1) {
       return;
     }
-    hSelectionCounter->Fill(5);
-    hPt->Fill(p.Pt());
-    hPx->Fill(p.Px());
-    hPy->Fill(p.Py());
-    hPz->Fill(p.Pz());
-    hRap->Fill(p.Rapidity());
-    hMass->Fill(p.M());
-    hPhi->Fill(p.Phi());
-    hEta->Fill(p1.Eta());
-    hEta->Fill(p2.Eta());
-    hPtsingle_muons->Fill(p1.Pt());
-    hPtsingle_muons->Fill(p2.Pt());
+    registry.fill(HIST("hSelectionCounter"), 7);
+    registry.fill(HIST("hPt"), p.Pt());
+    registry.fill(HIST("hPx"), p.Px());
+    registry.fill(HIST("hPy"), p.Py());
+    registry.fill(HIST("hPz"), p.Pz());
+    registry.fill(HIST("hRap"), p.Rapidity());
+    registry.fill(HIST("hMass"), p.M());
+    registry.fill(HIST("hPhi"), p.Phi());
+    registry.fill(HIST("hEta"), p1.Eta());
+    registry.fill(HIST("hEta"), p2.Eta());
+    registry.fill(HIST("hPtsingle"), p1.Pt());
+    registry.fill(HIST("hPtsingle"), p2.Pt());
+
   } //end of process
 };
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<UPCForward>(cfgc, TaskName{"upc-forward"})};
+    adaptAnalysisTask<UPCForward>(cfgc)};
 }
